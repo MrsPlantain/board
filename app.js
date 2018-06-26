4ae512adf1bca657270456927d0c4965f5e88f69
lucy <chaolj.516@gmail.com>
2018-06-26 09:26:08 +0800

    A simple of board

---
+++ app.js
@@ -0,0 +1,70 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require("body-parser");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+var entries = [];
+app.locals.entries = entries;
+
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/new-entry',function (request, response) {
+  if (request.method === 'GET') {
+    response.render('new-entry');
+  } else {
+      if (!request.body.title || !request.body.title) {
+          response.status(401).send("Entries must have a title and a body.");
+          return;
+      }
+
+      entries.push({
+          title : request.body.title,
+          body : request.body.body,
+          published : new Date()
+      });
+
+      response.redirect('/');
+  }
+});
+
+app.use(function (request, response) {
+
+    response.status(404).render('404');
+});
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+module.exports = app;
